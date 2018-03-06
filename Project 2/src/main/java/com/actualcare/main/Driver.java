0adf409e2242c0fc1dc0d9a93cf8a56b0f4d0bed
harish <harishkumarchandra@gamil.com>
2018-03-06 18:10:49 -0500

    angular

--- Project 2/src/main/java/com/actualcare/main/Driver.java
+++ Project 2/src/main/java/com/actualcare/main/Driver.java
@@ -1,14 +1,21 @@
 package com.actualcare.main;
 
+import com.actualcare.beans.Admin;
+import com.actualcare.beans.Doctor;
+import com.actualcare.beans.LoginInfo;
+import com.actualcare.beans.Patient;
 import com.actualcare.beans.Treatment;
-import com.actualcare.dao.TreatmentDaoImpl;
+import com.actualcare.dao.AdminDao;
+import com.actualcare.dao.DoctorDao;
+import com.actualcare.dao.PatientDao;
 import com.actualcare.dao.TreatmentDao;
+import com.actualcare.dao.TreatmentDaoImpl;
 
 public class Driver {
 
 	public static void main(String[] args) {
 		
-	/*	// Patient login data insert test
+		// Patient login data insert test
 		Patient p = new Patient((new LoginInfo("harish@com","harish")));
 		PatientDao.insertPatient(p);
 		
@@ -18,17 +25,17 @@ public class Driver {
 		
 		// Admin login data insert test
 		Admin a = new Admin((new LoginInfo("steven@com","steven")));
-		AdminDao.insertAdmin(a);*/
+		AdminDao.insertAdmin(a);
 		
 		// Treatement data insert test
-/*		Treatment t = new Treatment("test");
+		Treatment t = new Treatment("test");
 		TreatmentDao tDao = new TreatmentDaoImpl();
 		int id = tDao.insert(t);
 		System.out.println(id);
 		
 		Treatment t1 = tDao.returnTreatment(id);
 		System.out.println(t1.getT_id());
-		System.out.println(t.getT_id());*/
+		System.out.println(t.getT_id());
 
 	}
 
